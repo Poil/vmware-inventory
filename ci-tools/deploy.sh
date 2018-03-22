b9fbbdd8897d2cdd353c326ff0e59d856a8983ae
Benjamin DUPUIS <benjamin.dupuis@orange.com>
2018-03-22 09:01:31 +0100

    Gni et regni wrong port

--- ci-tools/deploy.sh
+++ ci-tools/deploy.sh
@@ -5,6 +5,6 @@ if [[ ${res} -eq 0 ]]; then
     docker stop vmware-api
     docker rm vmware-api
 fi
-docker run -d -v /etc/docker/vmware-api/config.yaml:/code/config.yaml -p 8003:8000 --restart=unless-stopped --name vmware-api vmware-api
+docker run -d -v /etc/docker/vmware-api/config.yaml:/code/config.yaml -p 8003:5000 --restart=unless-stopped --name vmware-api vmware-api
 
 
